2d7e7a9e317b209bb29c31f06108cf713956303d
Tianjian <tianjianqin@outlook.com>
2024-07-07 22:01:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = 3000;
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 
-// Serve static files from the project directory
-app.use(express.static(path.join(__dirname)));
+// Set up yargs for command line argument parsing and help
+const argv = yargs(hideBin(process.argv))
+    .usage('Usage: $0 --path <string> --port <number>')
+    .demandOption(['path', 'port'])
+    .describe('path', 'The path to the directory to serve files from')
+    .describe('port', 'The port to run the Express server on')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+const directoryPath = path.resolve(argv.path);
+const port = parseInt(argv.port, 10);
+
+const app = express();
+
+// Serve static files from the specified directory
+app.use(express.static(directoryPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'resume-float.html'));
+    res.sendFile(path.join(directoryPath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Serving files from ${directoryPath}`);
 });
